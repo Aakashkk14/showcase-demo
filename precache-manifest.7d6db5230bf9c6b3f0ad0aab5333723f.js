11eccb6ea39aafa48b929ef23d8a54d908498dbc
Aakash Kumar <aakash.kumar@katonic.ai>
2023-04-05 17:27:08 +0530

    Updates

---
+++ precache-manifest.7d6db5230bf9c6b3f0ad0aab5333723f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a44f346a509506ec15666421357f33f8",
+    "url": "/showcase/index.html"
+  },
+  {
+    "revision": "ae99b4fbf835f307015f",
+    "url": "/showcase/static/css/main.527dcb7f.chunk.css"
+  },
+  {
+    "revision": "6c562fb9996556062745",
+    "url": "/showcase/static/js/2.818e480e.chunk.js"
+  },
+  {
+    "revision": "ae99b4fbf835f307015f",
+    "url": "/showcase/static/js/main.edb02854.chunk.js"
+  },
+  {
+    "revision": "cff08fb0e10f392ff8c7",
+    "url": "/showcase/static/js/runtime-main.7e6d9ea2.js"
+  }
+]);
